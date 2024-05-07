a726a8e5a9ef5407658d9a95afcac8206bb2a3eb
itslukemango <itslukemango@gmail.com>
2024-05-07 10:51:08 +0100

    feat: add quantity arg to give command

--- src/main/java/com/lukemango/plotmines/commands/impl/GiveMineCommand.java
+++ src/main/java/com/lukemango/plotmines/commands/impl/GiveMineCommand.java
@@ -9,6 +9,7 @@ import com.sk89q.worldedit.command.util.CommandPermissions;
 import net.kyori.adventure.audience.Audience;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.incendo.cloud.annotations.Argument;
 import org.incendo.cloud.annotations.Command;
 import org.incendo.cloud.annotations.suggestion.Suggestions;
@@ -18,9 +19,12 @@ import java.util.List;
 
 public class GiveMineCommand extends AbstractCommand {
 
-    @Command("plotmines|plotmine|pmine|pm admin give <target> <mine>")
+    @Command("plotmines|plotmine|pmine|pm admin give <target> <mine> [amount]")
     @CommandPermissions("mangoplotmines.admin")
-    private void onGive(CommandSender sender, @Argument("target") Player target, @Argument(value = "mine", suggestions = "mine-items") String mine) {
+    private void onGive(CommandSender sender,
+                        @Argument("target") Player target,
+                        @Argument(value = "mine", suggestions = "mine-items") String mine,
+                        @Argument("amount") String amount) {
         final MineItem mineItem = ConfigManager.get().getConfig().getMineItem(sender, mine);
         if (mineItem == null) return; // If the mine item is null, the mine does not exist
 
@@ -33,16 +37,26 @@ public class GiveMineCommand extends AbstractCommand {
             return;
         }
 
-        // Give the player the mine item
-        target.getInventory().addItem(mineItem.creationItem());
+        final ItemStack item = mineItem.creationItem();
+        int intAmount;
+        try {
+            intAmount = Integer.parseInt(amount);
+        } catch (NumberFormatException e) {
+            intAmount = 1;
+        }
+        if (intAmount > 64) intAmount = 64; // Limit the amount to 64
+        item.setAmount(intAmount);
+        target.getInventory().addItem(item);
 
         // Send the messages to the player and the sender
         targetAudience.sendMessage(Colourify.colour(ConfigManager.get().getMessages().getPlayerReceivedMine()
-                .replace("<mine>", StringUtil.formatString(mine)))
+                .replace("<mine>", StringUtil.formatString(mine))
+                .replace("<amount>", String.valueOf(intAmount)))
         );
         senderAudience.sendMessage(Colourify.colour(ConfigManager.get().getMessages().getAdminMineGiven()
                 .replace("<player>", target.getName())
-                .replace("<mine>", StringUtil.formatString(mine)))
+                .replace("<mine>", StringUtil.formatString(mine))
+                .replace("<amount>", String.valueOf(intAmount)))
         );
     }
 
