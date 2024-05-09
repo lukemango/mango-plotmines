8ca1612f865583be7e43824506b49a37a1b519b1
itslukemango <itslukemango@gmail.com>
2024-05-09 16:27:14 +0100

    make mine storage async

--- src/main/java/com/lukemango/plotmines/storage/JsonMineStorage.java
+++ src/main/java/com/lukemango/plotmines/storage/JsonMineStorage.java
@@ -27,51 +27,56 @@ public class JsonMineStorage {
      * Save a mine to the JSON file
      */
     public void saveAll() {
-        try {
-            final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            final File file = new File(PlotMines.getInstance().getDataFolder().getAbsolutePath() + "/data/minedata.json");
+        PlotMines.getInstance().getServer().getScheduler().runTaskAsynchronously(PlotMines.getInstance(), () -> { // Save the mines asynchronously (non-blocking)
+            try {
+                final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                final File file = new File(PlotMines.getInstance().getDataFolder().getAbsolutePath() + "/data/minedata.json");
 
-            if (!file.exists()) {
-                file.getParentFile().mkdir(); // Creates the /data/
-                file.createNewFile(); // Creates the /data/minedata.json
-            }
+                if (!file.exists()) {
+                    file.getParentFile().mkdir(); // Creates the /data/
+                    file.createNewFile(); // Creates the /data/minedata.json
+                }
 
-            final Set<Mine> mines = MineManager.getMines();
-            if (mines != null) {
-                final Writer writer = new FileWriter(file, false);
-                gson.toJson(mines, writer);
-                writer.flush();
-                writer.close();
+                final Set<Mine> mines = MineManager.getMines();
+                if (mines != null) {
+                    final Writer writer = new FileWriter(file, false);
+                    gson.toJson(mines, writer);
+                    writer.flush();
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        });
     }
 
     /**
      * Load all mines from the JSON file
      */
     public void load() {
-        try {
-            final Gson gson = new Gson();
-            final File file = new File(PlotMines.getInstance().getDataFolder().getAbsolutePath() + "/data/minedata.json");
+        PlotMines.getInstance().getServer().getScheduler().runTaskAsynchronously(PlotMines.getInstance(), () -> {
+            try {
+                final Gson gson = new Gson();
+                final File file = new File(PlotMines.getInstance().getDataFolder().getAbsolutePath() + "/data/minedata.json");
 
-            if (!file.exists()) {
-                file.getParentFile().mkdir(); // Creates the /data/
-                file.createNewFile(); // Creates the /data/minedata.json
-            }
-
-            if (file.exists()) {
-                final Reader reader = new FileReader(file);
-                final TypeToken<Set<Mine>> typeToken = new TypeToken<>() {};
-                final Set<Mine> mineSet = gson.fromJson(reader, typeToken.getType());
-                if (mineSet != null) {
-                    MineManager.setMines(mineSet);
+                if (!file.exists()) {
+                    file.getParentFile().mkdir(); // Creates the /data/
+                    file.createNewFile(); // Creates the /data/minedata.json
                 }
+
+                if (file.exists()) {
+                    final Reader reader = new FileReader(file);
+                    final TypeToken<Set<Mine>> typeToken = new TypeToken<>() {
+                    };
+                    final Set<Mine> mineSet = gson.fromJson(reader, typeToken.getType());
+                    if (mineSet != null) {
+                        MineManager.setMines(mineSet);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        });
     }
 
     public static JsonMineStorage get() {
